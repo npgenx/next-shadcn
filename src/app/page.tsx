4a50c05108b8464cba95f108fe8ed0f0880223e7
JP Morgan <679434+npgenx@users.noreply.github.com>
2024-11-29 05:01:51 -0800

    fix image links for gh-pages deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/file.svg"
+            src="./file.svg"
             alt="File icon"
             width={16}
             height={16}
@@ -42,7 +42,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/window.svg"
+            src="./window.svg"
             alt="Window icon"
             width={16}
             height={16}
@@ -57,7 +57,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/globe.svg"
+            src="./globe.svg"
             alt="Globe icon"
             width={16}
             height={16}
